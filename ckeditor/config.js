4cfdf18c7c50c17699671242eba70871e5c7346e
sun <rajender.chandel@sun-softwares.com>
2014-06-10 11:00:28 +0530

    first commit

---
+++ ckeditor/config.js
@@ -0,0 +1,14 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.contentsCss = 'fonts.css';
+	//the next line add the new font to the combobox in CKEditor
+	config.font_names = 'Brush Script MT Kursiv;' + config.font_names;
+};
